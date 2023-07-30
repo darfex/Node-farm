3908fa8d2690294335df7cf1b911a5b588f71a77
darfex2020 <darfex2020@gmail.com>
2023-07-31 00:42:06 +0100

    Added routes

---
+++ index.js
@@ -0,0 +1,74 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+const replaceTemplate = require('./modules/replaceTemplate');
+
+// Blocking synchronous code
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+
+// const textOut = `This is what we know about avocados: ${textIn}. \n Created at ${Date.now()}`;
+
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File Created');
+
+// Non-blocking asynchronous code
+
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//     fs.readFile(`./ /${data1}.txt`, 'utf-8', (err, data2) => {
+//         fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, err => {
+//                 console.log("File written");
+//             });
+//         });
+//     });
+// });
+// console.log("Will read file");
+
+/** 
+ * Server
+ */
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+
+    const { query, pathname} = url.parse(req.url, true);
+
+    if(pathname === '/' || pathname === '/overview'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+        res.end(output);
+    }else if(pathname === '/product'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+
+        const product = dataObj[query.id];
+
+        console.log(product.nutrients);
+
+        const output = replaceTemplate(tempProduct, product);
+
+        res.end(output);
+    }else if(pathname === '/api'){
+
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(data)
+    }
+    else{
+        res.writeHead(404, {
+            'Content-Type': 'text/html'
+        });
+        res.end("<h1>Page not found</h1>");
+    }
+});
+
+server.listen(8000, () => {
+    console.log('Listening to requests on port 8000');
+});
